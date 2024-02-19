b08c5a936d4249625b345f90802ee424cd06c609
xiaomengjie <xiaomengjie2020@yeah.net>
2024-02-19 13:58:35 +0800

    ScalableImageView 简陋版

---
+++ app/src/main/java/com/xiao/today/basicdraw/touchevent/PointsRelayMoveView.kt
@@ -0,0 +1,10 @@
+package com.xiao.today.basicdraw.touchevent
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PointsRelayMoveView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
+
+
+}
--- app/src/main/java/com/xiao/today/basicdraw/touchevent/ScalableImageView.kt
+++ app/src/main/java/com/xiao/today/basicdraw/touchevent/ScalableImageView.kt
@@ -1,211 +1,251 @@
 package com.xiao.today.basicdraw.touchevent
 
-import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
+import android.view.ScaleGestureDetector.OnScaleGestureListener
 import android.view.View
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Interpolator
 import android.widget.OverScroller
-import androidx.core.view.GestureDetectorCompat
+import androidx.core.animation.doOnEnd
+import androidx.core.content.res.getDrawableOrThrow
+import androidx.core.graphics.drawable.toBitmapOrNull
+import androidx.core.view.ViewCompat
 import com.xiao.today.basicdraw.R
 import com.xiao.today.basicdraw.dp2px
-import com.xiao.today.basicdraw.loadAvatar
 
-class ScalableImageView(context: Context, attrs: AttributeSet?) : View(context, attrs),
-    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
-    private val defaultWidth = 300f.dp2px
+class ScalableImageView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private val defaultWidth = 300f.dp2px.toInt()
+
+    private val bitmap: Bitmap?
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private val bitmap: Bitmap
+    private var originalOffsetX = 0f
+    private var offsetX = 0f
+    private var originalOffsetY = 0f
+    private var offsetY = 0f
 
-    private var offsetX: Float = 0f
+    private var smallScale = 0f
+    private var bigScale = 0f
+
+    //是否支持缩放
+    private var scalable: Boolean = true
+
+    private val onGestureListener = OnGestureListener()
+
+    //手势检测工具类（双击、滑动等）
+    private val gestureDetector = GestureDetector(context, onGestureListener)
+    private val scaleGestureDetector = ScaleGestureDetector(context, onGestureListener)
+    private val flingRunnable = FlingRunnable()
+
+    private val overScroller = OverScroller(context)
+
+    //是否是大图显示
+    private var isBig = false
+
+    private var currentScale: Float = 0f
         set(value) {
             field = value
             invalidate()
         }
-    private var offsetY: Float = 0f
-        set(value) {
-            field = value
-            invalidate()
+
+    private val scaleAnimator = ObjectAnimator.ofFloat(
+        this, "currentScale", smallScale, bigScale
+    ).apply {
+        doOnEnd {
+            if (!isBig){
+                offsetX = 0f
+                offsetY = 0f
+            }
         }
-
-    private var smallScale: Float = 0f
-    private var bigScale: Float = 0f
-
-    var currentScale = 1f
-        set(value) {
-            field = value
-            invalidate()
-        }
-
-    private var enabledMove: Boolean = true
-
-    /**
-     * GestureDetectorCompat：手势监听器，监听用户各种操作（点击、滑动等）
-     *
-     */
-    private val gestureDetectorCompat = GestureDetectorCompat(context, this).apply {
-        setOnDoubleTapListener(this@ScalableImageView)
     }
-
-    private val overScroller = OverScroller(context, AccelerateDecelerateInterpolator())
 
     init {
-        bitmap = context.loadAvatar(R.drawable.avatar, defaultWidth.toInt())
+        val array = context.obtainStyledAttributes(attrs, R.styleable.ScalableImageView)
+        val drawable = array.getDrawableOrThrow(R.styleable.ScalableImageView_src)
+        //图片宽度
+        val width =
+            array.getDimensionPixelSize(R.styleable.ScalableImageView_image_width, defaultWidth)
+        scalable = array.getBoolean(R.styleable.ScalableImageView_scalable, true)
+        array.recycle()
+        //转换为bitmap
+        bitmap = drawable.toBitmapOrNull(
+            width = width,
+            height = (drawable.intrinsicHeight / drawable.intrinsicWidth.toFloat() * width).toInt()
+        )
     }
+
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        val bitmap = bitmap ?: return
+        //原始偏移，将bitmap绘制到view的中心
+        originalOffsetX = (width - bitmap.width) / 2f
+        originalOffsetY = (height - bitmap.height) / 2f
 
-        resetOffset()
-
-        calculateScale()
-    }
-
-    private fun calculateScale() {
-        val bitmapRadio = bitmap.width / bitmap.height.toFloat()
-        val viewRadio = width / height.toFloat()
-        if (bitmapRadio > viewRadio) {
-            smallScale = width / bitmap.width.toFloat()
-            bigScale = height / bitmap.height.toFloat()
+        //横图还是竖图
+        val bitmapRatio = bitmap.width / bitmap.height.toFloat()
+        val screenRatio = width / height.toFloat()
+        if (bitmapRatio > screenRatio) {
+            //横图
+            bigScale = height.toFloat() / bitmap.height * 1.5f
+            smallScale = width.toFloat() / bitmap.width
         } else {
-            smallScale = height / bitmap.height.toFloat()
-            bigScale = width / bitmap.width.toFloat()
+            //竖图
+            bigScale = width.toFloat() / bitmap.width * 1.5f
+            smallScale = height.toFloat() / bitmap.height
         }
+
+        currentScale = smallScale
+        scaleAnimator.setFloatValues(smallScale, bigScale)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val save = canvas.save()
-        canvas.translate(offsetX, offsetY)
-        canvas.scale(currentScale, currentScale, bitmap.width / 2f, bitmap.height / 2f)
-        canvas.drawBitmap(bitmap, 0f, 0f, paint)
-        canvas.restoreToCount(save)
+        val bitmap = bitmap ?: return
+        canvas.save()
+        val fraction = (currentScale - smallScale) / (bigScale - smallScale)
+        canvas.translate(offsetX * fraction, offsetY * fraction)
+        if (scalable) canvas.scale(currentScale, currentScale, width / 2f, height / 2f)
+        canvas.drawBitmap(bitmap, originalOffsetX, originalOffsetY, paint)
+        canvas.restore()
     }
 
-    private fun resetOffset() {
-        offsetX = (width - bitmap.width) / 2f
-        offsetY = (height - bitmap.height) / 2f
-    }
-
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        return gestureDetectorCompat.onTouchEvent(event)
-    }
-
-    /**
-     * ACTION_DOWN事件时回调，返回值表示是否消耗事件
-     */
-    override fun onDown(e: MotionEvent): Boolean {
-        return e.actionMasked == MotionEvent.ACTION_DOWN
-    }
-
-    /**
-     * 按下，100ms后触发
-     */
-    override fun onShowPress(e: MotionEvent) {
-    }
-
-    /**
-     * 点击（有长按监听时400ms以内触发）
-     */
-    override fun onSingleTapUp(e: MotionEvent): Boolean {
-        return false
-    }
-
-    /**
-     * 快速滑动
-     * velocityX、velocityY：速率
-     */
-    override fun onFling(
-        downEvent: MotionEvent?,
-        e2: MotionEvent,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        return false
-    }
-
-    /**
-     * 相当于onMove：手指移动时调用
-     *
-     * distanceX，distanceY：移动距离（move1 - move2）
-     */
-    override fun onScroll(
-        downEvent: MotionEvent?,
-        currentEvent: MotionEvent,
-        distanceX: Float,
-        distanceY: Float
-    ): Boolean {
-        return if (enabledMove){
-            offsetX -= distanceX
-            offsetY -= distanceY
-            offsetX = when{
-                offsetX <= 0 -> 0f
-                offsetX >= width - bitmap.width -> (width - bitmap.width).toFloat()
-                else -> offsetX
-            }
-            offsetY = when{
-                offsetY <= 0 -> 0f
-                offsetY >= height - bitmap.height -> (height - bitmap.height).toFloat()
-                else -> offsetY
-            }
-            true
-        }else{
-            false
+        val touchEvent = scaleGestureDetector.onTouchEvent(event)
+        if (!scaleGestureDetector.isInProgress){
+            return gestureDetector.onTouchEvent(event)
         }
+        return touchEvent
     }
 
     /**
-     * 长按：
+     * 控制最大最小偏移
      */
-    override fun onLongPress(e: MotionEvent) {
-        if (enabledMove){
-            val scaleAnimator =
-                ObjectAnimator.ofFloat(this, "currentScale", currentScale, 1f)
-            val offsetXAnimator =
-                ObjectAnimator.ofFloat(this, "offsetX", offsetX, (width - bitmap.width) / 2f)
-            val offsetYAnimator =
-                ObjectAnimator.ofFloat(this, "offsetY", offsetY, (height - bitmap.height) / 2f)
-            AnimatorSet().apply {
-                playTogether(
-                    scaleAnimator, offsetXAnimator, offsetYAnimator
+    private fun fixOffset() {
+        if (bitmap == null) return
+        val maxWidth = (bigScale * bitmap.width - width) / 2f
+        val maxHeight = (bigScale * bitmap.height - height) / 2f
+        offsetX = minOf(offsetX, maxWidth)
+        offsetX = maxOf(offsetX, -maxWidth)
+        offsetY = minOf(offsetY, maxHeight)
+        offsetY = maxOf(offsetY, -maxHeight)
+        invalidate()
+    }
+
+    private inner class OnGestureListener : SimpleOnGestureListener(), OnScaleGestureListener {
+        /**
+         * 快速滑动
+         */
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            if (isBig) {
+                if (bitmap == null) return false
+                overScroller.fling(
+                    offsetX.toInt(), offsetY.toInt(), velocityX.toInt(), velocityY.toInt(),
+                    (-(bitmap.width * bigScale - width) / 2f).toInt(),
+                    ((bitmap.width * bigScale - width) / 2f).toInt(),
+                    (-(bitmap.height * bigScale - height) / 2f).toInt(),
+                    ((bitmap.height * bigScale - height) / 2f).toInt(),
+                    bitmap.width / 2, bitmap.height / 2
                 )
-            }.start()
+                //下一帧调用（相当于延时message）
+                ViewCompat.postOnAnimation(this@ScalableImageView, flingRunnable)
+            }
+            return true
         }
-    }
 
+        /**
+         * 移动
+         */
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            if (isBig) {
+                offsetX -= distanceX
+                offsetY -= distanceY
+                fixOffset()
+            }
+            return true
+        }
 
+        /**
+         * 双击
+         */
+        override fun onDoubleTap(event: MotionEvent): Boolean {
+            isBig = !isBig
+            if (isBig) {
+                //点击时初始位移
+                //防止因为offsetX和offsetY突然改变跳动
+                if (offsetX == 0f && offsetY == 0f){
+                    offsetX = (event.x - width / 2f) * (1 - bigScale / smallScale)
+                    offsetY = (event.y - height / 2f) * (1 - bigScale / smallScale)
+                    fixOffset()
+                }
+                scaleAnimator.setFloatValues(currentScale, bigScale)
+                scaleAnimator.start()
+            } else {
+                scaleAnimator.setFloatValues(smallScale, bigScale)
+                scaleAnimator.reverse()
+            }
+            return true
+        }
 
+        override fun onDown(event: MotionEvent): Boolean {
+            return event.actionMasked == MotionEvent.ACTION_DOWN
+        }
 
-    override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
-        return false
-    }
-
-    override fun onDoubleTap(e: MotionEvent): Boolean {
-        val destinationScale = when (currentScale) {
-            1f -> smallScale
-            smallScale -> bigScale
-            else -> {
-                enabledMove = true
-                bigScale * 1.5f
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            val tempScale = currentScale * detector.scaleFactor
+            return if (tempScale > bigScale || tempScale < smallScale){
+                false
+            }else{
+                currentScale = tempScale
+                true
             }
         }
-        val animator =
-            ObjectAnimator.ofFloat(this, "currentScale", currentScale, destinationScale)
-        animator.start()
-        return true
+
+        override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+            offsetX = (detector.focusX - width / 2f) * (1 - bigScale / smallScale)
+            offsetY = (detector.focusY - height / 2f) * (1 - bigScale / smallScale)
+            fixOffset()
+            return true
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector) {
+
+        }
     }
 
-    override fun onDoubleTapEvent(e: MotionEvent): Boolean {
-        return false
+    private inner class FlingRunnable : Runnable {
+        override fun run() {
+            if (overScroller.computeScrollOffset()) {
+                //计算距离，设置偏移
+                offsetX = overScroller.currX.toFloat()
+                offsetY = overScroller.currY.toFloat()
+                invalidate()
+                ViewCompat.postOnAnimation(this@ScalableImageView, this)
+            }
+        }
     }
 }
+
+// TODO: 1、放大后再次缩放跳动问题（offsetX和offsetY突变导致）
